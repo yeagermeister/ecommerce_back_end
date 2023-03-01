1457b782a2e9ca36e26390298db1603a28721cac
unknown <johnyeager75@gmail.com>
2023-03-01 12:34:18 -0500

    Models built

--- models/index.js
+++ models/index.js
@@ -5,12 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.hasOne(Category, {
+
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+
+});
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: 'ProductTag',
+  foreignKey: 'id'
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: 'ProductTag',
+  foreignKey: 'id'
+});
 
 module.exports = {
   Product,
