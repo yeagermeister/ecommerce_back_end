17b3fd6742357049ae1cabb05c3222f83efecd38
yeagermeister <johnyeager75@gmail.com>
2023-03-11 10:45:14 -0500

    Got all tables synchronizing properly

--- models/index.js
+++ models/index.js
@@ -5,24 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.hasOne(Category, {
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  // onDelete: 'CASCADE'
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-
+  foreignKey: 'category_id',
+  // onDelete: 'CASCADE'
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: 'ProductTag',
-  foreignKey: 'id'
+  through: 'product_tag',
+  foreignKey: 'product_id'
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: 'ProductTag',
-  foreignKey: 'id'
+  through: 'product_tag',
+  foreignKey: 'tag_id'
 });
 
 module.exports = {
